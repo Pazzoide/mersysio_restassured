5b2fea28bab72c27f8b0e3a86353d6843f1c8192
Ramazaninceli <inceliramazan80@gmail.com>
2023-11-09 19:22:29 +0300

    Taskımı tamamladım

--- src/test/java/Campus_API_Testing/_008_Departments_.java
+++ src/test/java/Campus_API_Testing/_008_Departments_.java
@@ -1,4 +1,128 @@
 package Campus_API_Testing;
 
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class _008_Departments_ extends _000_Login_{
+
+
+    String departmentsID="";
+
+    String rndName="";
+    String rndCode="";
+    String schoolId="646cbb07acf2ee0d37c6d984";
+
+
+
+    @Test
+    public void createDepartments(){
+
+        rndName= faker.country().currencyCode()+faker.number().digits(2);
+
+        Map<String,String> newDepartments=new HashMap<>();
+      newDepartments.put("name",rndName);
+      newDepartments.put("code",faker.number().digits(4));
+      newDepartments.put("school",schoolId);
+
+      departmentsID=
+                given()
+                        .spec(reqSpec)
+                        .body(newDepartments)
+                       // .log().body()
+                        .when()
+                        .post("school-service/api/department")
+
+                        .then()
+                        //.log().body()
+                       .statusCode(201)
+                        .extract().path("id");
+
+                   }
+            @Test(dependsOnMethods = "createDepartments")
+    public void createDepartmentsNegative(){
+
+                Map<String,String> newDepartments=new HashMap<>();
+                newDepartments.put("name",rndName);
+                newDepartments.put("code",faker.number().digits(4));
+                newDepartments.put("school",schoolId);
+
+
+                        given()
+                                .spec(reqSpec)
+                                .body(newDepartments)
+                                // .log().body()
+                                .when()
+                                .post("school-service/api/department")
+
+                                .then()
+                                //.log().body()
+                                .statusCode(400)
+                                .extract().path("id");
+                            }
+
+@Test(dependsOnMethods = "createDepartmentsNegative")
+public void updateParameters(){
+
+    rndName= faker.country().currencyCode()+faker.number().digits(2);
+
+    Map<String,String> newDepartments=new HashMap<>();
+    newDepartments.put("name",rndName);
+    newDepartments.put("id",departmentsID);
+    newDepartments.put("code",faker.number().digits(4));
+    newDepartments.put("school",schoolId);
+
+    given()
+            .spec(reqSpec)
+            .body(newDepartments)
+            // .log().body()
+            .when()
+            .put("school-service/api/department")
+
+            .then()
+            //.log().body()
+            .statusCode(200);
+
+}
+@Test(dependsOnMethods = "updateParameters")
+    public void delete(){
+    given()
+            .spec(reqSpec)
+            // .log().body()
+            .when()
+            .delete("school-service/api/department/"+departmentsID)
+
+            .then()
+            //.log().body()
+            .statusCode(204);
+
+    }
+    @Test(dependsOnMethods = "delete")
+    public void deleteNegative(){
+        given()
+                .spec(reqSpec)
+                // .log().body()
+                .when()
+                .delete("school-service/api/department/"+departmentsID)
+
+                .then()
+                //.log().body()
+                .statusCode(400);
+
+           }
+
+
+
+
 }
--- src/test/java/Campus_API_Testing/_009_Bank_Account.java
+++ src/test/java/Campus_API_Testing/_009_Bank_Account.java
@@ -1,4 +1,127 @@
 package Campus_API_Testing;
 
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
 public class _009_Bank_Account extends _000_Login_{
+
+    Map<String, String> bankAccount;
+    String bankAccountUserName;
+    String schoolID="646cbb07acf2ee0d37c6d984";
+    String bankAccountID;
+
+    @Test
+    public void createBankAccount() {
+
+        bankAccount=new HashMap<>();
+
+        bankAccountUserName= faker.address().firstName()+" "+faker.address().lastName();
+        bankAccount.put("name", bankAccountUserName);
+
+        bankAccount.put("iban", "Team12" + faker.number().digits(12));
+        bankAccount.put("integrationCode", faker.number().digits(4));
+
+        bankAccount.put("currency", "EUR");
+        bankAccount.put("schoolId", schoolID);
+
+
+        bankAccountID =
+
+                given()
+
+                        .spec(reqSpec)
+                        .body(bankAccount)
+                        //.log().body()
+                        .when()
+                        .post("school-service/api/bank-accounts")
+                        .then()
+                        //.log().body()
+                        .statusCode(201)
+                        .extract().path("id");
+
+
+    }
+@Test(dependsOnMethods = "createBankAccount")
+    public void createBankAccountNegative() {
+
+        given()
+
+                .spec(reqSpec)
+                .body(bankAccount)
+                //.log().body()
+                .when()
+                .post("school-service/api/bank-accounts")
+                .then()
+                //.log().body()
+                .statusCode(400)
+                .body("message", containsString("already")) ;
+
+
+
+    }
+@Test(dependsOnMethods = "createBankAccountNegative")
+    public void updateBankAccount() {
+
+        bankAccountUserName= faker.address().firstName()+" "+faker.address().lastName()+" "+faker.address().lastName();
+        bankAccount.put("name", bankAccountUserName);
+
+        bankAccount.put("iban", "DE" + faker.number().digits(16));
+        bankAccount.put("integrationCode", faker.number().digits(8));
+
+        bankAccount.put("currency", "USD");
+        bankAccount.put("schoolId", schoolID);
+        bankAccount.put("id",bankAccountID);
+
+        given()
+
+                .spec(reqSpec)
+                .body(bankAccount)
+                // .log().body()
+                .when()
+                .put("school-service/api/bank-accounts")
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("name", equalTo(bankAccountUserName));
+
+
+
+    }
+    @Test(dependsOnMethods = "updateBankAccount")
+    public void deleteBankAccount() {
+
+        given()
+
+                .spec(reqSpec)
+                .when()
+                .delete("school-service/api/bank-accounts/" + bankAccountID)
+                .then()
+                //.log().body()
+                .statusCode(200);
+
+
+    }
+    @Test(dependsOnMethods = "deleteBankAccount")
+    public void deleteBankAccountNegative() {
+
+        given()
+
+                .spec(reqSpec)
+                //.log().uri()
+                .when()
+                .delete("school-service/api/bank-accounts/" + bankAccountID)
+                .then()
+                //.log().body()
+                .statusCode(400)
+                .body("message", containsString("must be exist"));
+
+
+    }
+
 }
